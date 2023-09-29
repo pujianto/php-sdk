85bb28cc6707b00527f587be1f9673c41eb44381
Pujianto <pujianto3mail@gmail.com>
2023-09-29 12:49:11 +0700

    test: add LocationTest

--- tests/Unit/LocationTest.php
+++ tests/Unit/LocationTest.php
@@ -1,11 +1,56 @@
 <?php
+
 namespace Webdock\Tests\Unit;
+
 use PHPUnit\Framework\TestCase;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
 use Webdock\Client as WebdockClient;
 use GuzzleHttp\Psr7\Response;
+use Webdock\Tests\Helper;
 
-class LocationTest extends TestCase {
-  
+/**
+ * 
+ * @testdox Possible server locations
+ * 
+ */
+class LocationTest extends TestCase
+{
+
+
+    /**
+     * 
+     * @dataProvider locationListResponse
+     */
+    public function testLocationList($serverResponse)
+    {
+        $handler = Helper::createMockHandler([
+            new Response(200, [], json_encode($serverResponse))
+        ]);
+
+        $client = new WebdockClient(
+            Helper::token(),
+            Helper::appName(),
+            $handler
+        );
+
+        $response = $client->location->list()
+            ->getResponse()->toArray();
+
+        $this->assertSame($serverResponse, $response);
+    }
+
+    public function locationListResponse()
+    {
+        return [
+            [
+                [
+                    "id" => "string",
+                    "name" => "string",
+                    "city" => "string",
+                    "country" => "string",
+                    "description" => "string",
+                    "icon" => "string"
+                ]
+            ]
+        ];
+    }
 }
