b4dcb5f7899f909c164fc313d615fbaad0903621
Pujianto <pujianto3mail@gmail.com>
2023-09-29 12:45:28 +0700

    test: add ImageTest

--- tests/Unit/ImageTest.php
+++ tests/Unit/ImageTest.php
@@ -1,11 +1,53 @@
 <?php
+
 namespace Webdock\Tests\Unit;
+
 use PHPUnit\Framework\TestCase;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
 use Webdock\Client as WebdockClient;
 use GuzzleHttp\Psr7\Response;
+use Webdock\Tests\Helper;
 
-class ImageTest extends TestCase {
-  
+/**
+ * 
+ * @testdox Possible server images
+ * 
+ */
+class ImageTest extends TestCase
+{
+
+    /**
+     * 
+     * @dataProvider imageListResponse
+     */
+    public function testImageList($serverResponse)
+    {
+        $handler = Helper::createMockHandler([
+            new Response(200, [], json_encode($serverResponse))
+        ]);
+
+        $client = new WebdockClient(
+            Helper::token(),
+            Helper::appName(),
+            $handler
+        );
+
+        $response = $client->image->list()
+            ->getResponse()->toArray();
+
+        $this->assertSame($serverResponse, $response);
+    }
+
+    public function imageListResponse()
+    {
+        return [
+            [
+                [
+                    "slug" => "string",
+                    "name" => "string",
+                    "webServer" => null,
+                    "phpVersion" => "string"
+                ]
+            ]
+        ];
+    }
 }
