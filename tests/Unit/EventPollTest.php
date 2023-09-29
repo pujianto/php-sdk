57052d3d8c61a8c5bb0288d767cc45bbde4124c5
Pujianto <pujianto3mail@gmail.com>
2023-09-29 12:36:25 +0700

    test: add EventPollTest

--- tests/Unit/EventPollTest.php
+++ tests/Unit/EventPollTest.php
@@ -1,11 +1,66 @@
 <?php
+
 namespace Webdock\Tests\Unit;
+
 use PHPUnit\Framework\TestCase;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
 use Webdock\Client as WebdockClient;
 use GuzzleHttp\Psr7\Response;
+use Webdock\Tests\Helper;
 
-class EventPollTest extends TestCase {
-  
+
+/**
+ * 
+ * @testdox Event Poll
+ * 
+ */
+
+class EventPollTest extends TestCase
+{
+
+    /**
+     * 
+     * @dataProvider eventPollListResponse
+     */
+    public function testEventPollList($serverResponse)
+    {
+        $handler = Helper::createMockHandler([
+            new Response(200, [], json_encode($serverResponse))
+        ]);
+
+        $client = new WebdockClient(
+            Helper::token(),
+            Helper::appName(),
+            $handler
+        );
+
+        $params = [
+            'callbackId' => 'the-callback-id',
+            'eventType' => 'string',
+
+        ];
+        $response = $client->eventPoll->list($params)
+            ->getResponse()->toArray();
+
+        $this->assertSame($serverResponse, $response);
+    }
+
+    public function eventPollListResponse()
+    {
+        return [
+            [
+                [
+                    "id" => 0,
+                    "startTime" => "string",
+                    "endTime" => "string",
+                    "callbackId" => "string",
+                    "serverSlug" => "string",
+                    "eventType" => "string",
+                    "action" => "string",
+                    "actionData" => "string",
+                    "status" => "waiting",
+                    "message" => "string"
+                ]
+            ]
+        ];
+    }
 }
