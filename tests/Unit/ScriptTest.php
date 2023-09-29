cba0d3058ba59eaa84cf4e9f34b3cb0ebf20d209
Pujianto <pujianto3mail@gmail.com>
2023-09-29 13:10:44 +0700

    test: add ScriptTest

--- tests/Unit/ScriptTest.php
+++ tests/Unit/ScriptTest.php
@@ -1,11 +1,59 @@
 <?php
+
 namespace Webdock\Tests\Unit;
+
 use PHPUnit\Framework\TestCase;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
 use Webdock\Client as WebdockClient;
 use GuzzleHttp\Psr7\Response;
+use Webdock\Tests\Helper;
 
-class ScriptTest extends TestCase {
-  
+/**
+ * 
+ * @testdox Script Library
+ * 
+ */
+class ScriptTest extends TestCase
+{
+
+    /**
+     * 
+     * @dataProvider scriptListResponse
+     */
+    public function testScriptList($serverResponse)
+    {
+        $handler = Helper::createMockHandler([
+            new Response(200, [], json_encode($serverResponse))
+        ]);
+
+        $client = new WebdockClient(
+            Helper::token(),
+            Helper::appName(),
+            $handler
+        );
+
+        $response = $client->script
+            ->list()->getResponse()->toArray();
+
+        $this->assertSame(
+            $serverResponse,
+            $response
+        );
+    }
+
+    public function scriptListResponse()
+    {
+        return
+            [
+
+                [
+                    [
+                        "id" => 0,
+                        "name" => "string",
+                        "description" => "string",
+                        "filename" => "string",
+                        "content" => "string"
+                    ]
+                ]
+            ];
+    }
 }
