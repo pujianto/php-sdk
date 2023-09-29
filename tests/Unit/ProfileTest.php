e876f7055b2a3f7de73753b14ccbc064a733144c
Pujianto <pujianto3mail@gmail.com>
2023-09-29 12:54:08 +0700

    test: add ProfileTest

--- tests/Unit/ProfileTest.php
+++ tests/Unit/ProfileTest.php
@@ -1,11 +1,64 @@
 <?php
+
 namespace Webdock\Tests\Unit;
+
 use PHPUnit\Framework\TestCase;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
 use Webdock\Client as WebdockClient;
 use GuzzleHttp\Psr7\Response;
+use Webdock\Tests\Helper;
 
-class ProfileTest extends TestCase {
-  
+/**
+ * 
+ * @testdix Possible server profiles
+ * 
+ */
+class ProfileTest extends TestCase
+{
+
+    /**
+     * 
+     * @dataProvider profileListResponse 
+     */
+    public function testProfileList($serverResponse)
+    {
+        $handler = Helper::createMockHandler([
+            new Response(200, [], json_encode($serverResponse))
+        ]);
+
+        $client = new WebdockClient(
+            Helper::token(),
+            Helper::appName(),
+            $handler
+        );
+
+
+        $response = $client->profile->list('europe')
+            ->getResponse()->toArray();
+
+        $this->assertSame($serverResponse, $response);
+    }
+
+    public function profileListResponse()
+    {
+        return
+            [
+                [
+                    [
+                        "slug" => "string",
+                        "name" => "string",
+                        "ram" => 0,
+                        "disk" => 0,
+                        "cpu" => [
+                            "cores" => 0,
+                            "threads" => 0
+                        ],
+                        "price" => [
+                            "amount" => 0,
+                            "currency" => "EUR"
+                        ]
+                    ]
+                ]
+
+            ];
+    }
 }
