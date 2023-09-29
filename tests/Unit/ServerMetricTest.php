e44b2748e95561fd4ca6cd909b971ca8ecf50669
Pujianto <pujianto3mail@gmail.com>
2023-09-29 14:11:55 +0700

    test: add ServerMetricTest

--- tests/Unit/ServerMetricTest.php
+++ tests/Unit/ServerMetricTest.php
@@ -1,11 +1,171 @@
 <?php
+
 namespace Webdock\Tests\Unit;
+
 use PHPUnit\Framework\TestCase;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
 use Webdock\Client as WebdockClient;
 use GuzzleHttp\Psr7\Response;
+use Webdock\Tests\Helper;
 
-class ServerMetricTest extends TestCase {
-  
+/**
+ * 
+ * @testdox Server Metrics
+ * 
+ */
+class ServerMetricTest extends TestCase
+{
+
+    /**
+     * 
+     * @dataProvider getMetricsResponse
+     */
+    public function testGetMetrics($serverResponse)
+    {
+        $handler = Helper::createMockHandler([
+            new Response(200, [], json_encode($serverResponse))
+        ]);
+
+        $client = new WebdockClient(
+            Helper::token(),
+            Helper::appName(),
+            $handler
+        );
+
+        $response = $client->serverMetric->getMetrics('server-slug')
+            ->getResponse()->toArray();
+
+        $this->assertSame($serverResponse, $response);
+    }
+
+    public function getMetricsResponse()
+    {
+        return
+            [
+                [
+                    [
+                        "disk" => [
+                            "allowed" => 0,
+                            "samplings" => [
+                                [
+                                    "amount" => 0,
+                                    "timestamp" => "string"
+                                ]
+                            ]
+                        ],
+                        "network" => [
+                            "total" => 0,
+                            "allowed" => 0,
+                            "ingressSamplings" => [
+                                [
+                                    "amount" => 0,
+                                    "timestamp" => "string"
+                                ]
+                            ],
+                            "egressSamplings" => [
+                                [
+                                    "amount" => 0,
+                                    "timestamp" => "string"
+                                ]
+                            ]
+                        ],
+                        "cpu" => [
+                            "usageSamplings" => [
+                                [
+                                    "amount" => 0,
+                                    "timestamp" => "string"
+                                ]
+                            ]
+                        ],
+                        "processes" => [
+                            "processesSamplings" => [
+                                [
+                                    "amount" => 0,
+                                    "timestamp" => "string"
+                                ]
+                            ]
+                        ],
+                        "memory" => [
+                            "usageSamplings" => [
+                                [
+                                    "amount" => 0,
+                                    "timestamp" => "string"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+
+            ];
+    }
+
+    /**
+     * 
+     * @dataProvider instantMetricsResponse
+     */
+    public function testGetInstantMetrics($serverResponse)
+    {
+        $handler = Helper::createMockHandler([
+            new Response(200, [], json_encode($serverResponse))
+        ]);
+
+        $client = new WebdockClient(
+            Helper::token(),
+            Helper::appName(),
+            $handler
+        );
+
+        $response = $client->serverMetric->getMetricsNow('server-slug')
+            ->getResponse()->toArray();
+
+        $this->assertSame($serverResponse, $response);
+
+    }
+
+    public function instantMetricsResponse()
+    {
+        return
+            [
+                [
+                    [
+                        "disk" => [
+                            "allowed" => 0,
+                            "lastSamplings" => [
+                                "amount" => 0,
+                                "timestamp" => "string"
+                            ]
+                        ],
+                        "network" => [
+                            "total" => 0,
+                            "allowed" => 0,
+                            "latestIngressSampling" => [
+                                "amount" => 0,
+                                "timestamp" => "string"
+                            ],
+                            "latestEgressSampling" => [
+                                "amount" => 0,
+                                "timestamp" => "string"
+                            ]
+                        ],
+                        "cpu" => [
+                            "latestUsageSampling" => [
+                                "amount" => 0,
+                                "timestamp" => "string"
+                            ]
+                        ],
+                        "processes" => [
+                            "latestProcessesSampling" => [
+                                "amount" => 0,
+                                "timestamp" => "string"
+                            ]
+                        ],
+                        "memory" => [
+                            "latestUsageSampling" => [
+                                "amount" => 0,
+                                "timestamp" => "string"
+                            ]
+                        ]
+                    ]
+                ]
+            ];
+    }
 }
